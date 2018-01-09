eb24ab083d7f2ecf0e70463756fe6c79efe46624
leiys13 <leiys13@icloud.com>
2018-01-09 14:51:19 +0800

    用户控制器中添加登录方法

--- src/main/java/com/sadasen/account/user/controller/UserController.java
+++ src/main/java/com/sadasen/account/user/controller/UserController.java
@@ -1,10 +1,21 @@
 package com.sadasen.account.user.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @date 2018年1月9日
  * @author lei.ys
  * @desc
  */
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
 }
