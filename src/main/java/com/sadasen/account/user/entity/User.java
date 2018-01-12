57e92cc2a8bb6bc57e14a6a379b878270d49e3c1
leiys13 <leiys13@icloud.com>
2018-01-12 20:27:03 +0800

    用户类增加基本属性

--- src/main/java/com/sadasen/account/user/entity/User.java
+++ src/main/java/com/sadasen/account/user/entity/User.java
@@ -3,8 +3,39 @@ package com.sadasen.account.user.entity;
 /**
  * @date 2018年1月9日
  * @author lei.ys
- * @desc
+ * @desc  用户信息
  */
 public class User {
+	
+	private long id;
+	private String userName;
+	private String password;
 
+	public User() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
