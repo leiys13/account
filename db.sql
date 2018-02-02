ec4e947c160dc04c81a5fab5fd249a912eb46629
leiys13 <leiys13@icloud.com>
2018-02-02 09:32:31 +0800

    ibeet-SQL集成，数据源类配置

--- db.sql
+++ db.sql
@@ -1,7 +1,45 @@
 CREATE TABLE sys_user (
-	id int(11) primary key auto_increment,
+	id bigint(11) primary key AUTO_INCREMENT,
 	userName varchar(30) not null,
 	nickName varchar(30) default null,
 	password varchar(50) not null,
 	regTime datetime not null
 ) ENGINE = INNODB DEFAULT CHARSET = utf8;
+
+alter table sys_user AUTO_INCREMENT=11328156;
+
+CREATE TABLE account_consume (
+	id bigint(20) primary key AUTO_INCREMENT,
+	name varchar(30) default null,
+	pid bigint(20) default 0,
+	uid bigint(20) default 0,
+	code varchar(200) default null,
+	level tinyint(4) default 0,
+	sort tinyint(4) default 0,
+	create_time datetime default null
+) ENGINE = INNODB DEFAULT CHARSET = utf8;
+
+alter table account_consume AUTO_INCREMENT=11328156;
+
+CREATE TABLE account_method (
+	id bigint(20) primary key AUTO_INCREMENT,
+	method varchar(30) default null,
+	uid bigint(20) default 0,
+	sort tinyint(4) default 0,
+	create_time datetime default null
+) ENGINE = INNODB DEFAULT CHARSET = utf8;
+
+alter table account_method AUTO_INCREMENT=11328156;
+
+CREATE TABLE account_record (
+	id bigint(20) primary key AUTO_INCREMENT,
+	rdate date default null,
+	uid bigint(20) default 0,
+	cid bigint(20) default 0,
+	mid bigint(20) default 0,
+	price decimal(20,0) default 0,
+	remark varchar(200) default null,
+	create_time datetime default null
+) ENGINE = INNODB DEFAULT CHARSET = utf8;
+
+alter table account_record AUTO_INCREMENT=11328156;
---
+++ src/main/java/com/sadasen/account/config/DataSourceConfig.java
@@ -0,0 +1,29 @@
+package com.sadasen.account.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+/**
+ * @date 2018年2月2日
+ * @author lei.ys
+ * @desc
+ */
+@Configuration
+public class DataSourceConfig {
+	
+	@Bean(name="dataSource")
+	public DataSource dataSource(Environment env) {
+		HikariDataSource ds = new HikariDataSource();
+		ds.setJdbcUrl(env.getProperty("spring.datasource.url"));
+		ds.setUsername(env.getProperty("spring.datasource.username"));
+		ds.setPassword(env.getProperty("spring.datasource.password"));
+		ds.setDriverClassName(env.getProperty("spring.datasource.driverClassName"));
+		return ds;
+	}
+
+}
